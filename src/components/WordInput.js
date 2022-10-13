21fa94bc45c96d4c13598aff79c56d003340745b
Ritik Gupta <61592876+ritik955@users.noreply.github.com>
2022-10-13 16:02:05 +0530

    Update WordInput.js

--- src/components/WordInput.js
+++ src/components/WordInput.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export const WordInput = () => {
+export const WordInput = ({handleChange,value}) => {
     return (
         <>   
             <label htmlFor="word-input">Enter word to be highlighted here</label>
-            <input id="word-input" type={"text"}/>
+            <input id="word-input" type={"text"} onChange={(e)=>handleChange(e.target.value)} value={value}/>
         </>
     )
 }
