68a4f59b6cdf32d0d387f3bc54fa5f1268490750
Ritik Gupta <61592876+ritik955@users.noreply.github.com>
2022-10-13 16:02:47 +0530

    Update IgnoreCaseToggle.js

--- src/components/IgnoreCaseToggle.js
+++ src/components/IgnoreCaseToggle.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export const IgnoreCaseToggle = () => {
+export const IgnoreCaseToggle = ({checked,onChange}) => {
     return (
         <>
             <label htmlFor="ignore-case">Ignore case?</label>
-            <input type="checkbox" id="ignore-case"/>
+            <input type="checkbox" id="ignore-case" checked={checked} onChange={(e)=>onChange(!checked)} />
         </>
     )
 }
